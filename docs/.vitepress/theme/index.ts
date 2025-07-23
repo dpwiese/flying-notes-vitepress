7d1376f1d7f8888de24424ad073e526d32472cd1
Daniel Wiese <dpwiese@gmail.com>
2025-07-23 08:58:24 -0500

    hack to scroll to hash correctly

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -38,6 +38,34 @@ const theme: Theme = {
         MJ.startup?.promise ? MJ.startup.promise.then(run) : run();
       };
 
+      // This nasty helper function is a quick hack to solve what seems to be a
+      // bug best described in https://github.com/vuejs/vitepress/issues/3428.
+      // When navigating to a part of a page by clicking items in the sideline
+      // bar, it works as expected, including writing the hash to the URL.
+      // However, when attempting to navigate to a section of the page via a URL
+      // with hash, it scrolls somewhere weird. Other attempts to "manually"
+      // scroll to the desired portion of the page were ugly, fragile, and did
+      // not really work. This hack is designed to just automatically "click"
+      // on the link associated with the hash in the URL to properly take us to
+      // the right place on the page.
+      const scrollToHash = () => {
+        if (!location.hash) return;
+
+        nextTick(() => {
+          const id = decodeURIComponent(location.hash.slice(1));
+
+          // Find the sidebar links that look like <a href="#my-heading">...</a>
+          const link = document.querySelector(
+            `.VPDocAsideOutline a[href="#${CSS.escape(id)}"]`
+          ) as HTMLAnchorElement | null;
+
+          if (link) {
+            link.click();
+            return;
+          }
+        });
+      };
+
       // Helper: modify outline titles to omit anything within parentheses
       const modifyOutlineLinks = () => {
         nextTick(() => {
@@ -62,14 +90,18 @@ const theme: Theme = {
       // Run on initial load
       onMounted(() => {
         modifyOutlineLinks();
+        // Shameful hack
+        setTimeout(scrollToHash, 10);
       });
 
-      // Run on every route change
+      // Run on every route or hash change
       watch(
-        () => route.path,
+        () => [route.path, route.hash],
         () => {
           nextTick(retypeset);
           modifyOutlineLinks();
+          // Shameful hack
+          setTimeout(scrollToHash, 10);
         }
       );
 
